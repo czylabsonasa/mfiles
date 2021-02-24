5af98b49bf0b555915789d4d98991048be7b9f3f
czylabsonasa <czylabson.asa@gmail.com>
2021-02-24 10:20:15 +0100

    progress

--- statSzg/plotTest3.R
+++ statSzg/plotTest3.R
@@ -1,5 +1,6 @@
 xx = seq(-pi,pi,0.1)
 
+
 plot(
   xx, cos(xx),
   xlim=c(-4,4),
@@ -11,6 +12,7 @@ plot(
 )
 
 par(new=TRUE)
+
 plot(
   xx, cos(2*xx),
   xlim=c(-4,4),
@@ -21,8 +23,19 @@ plot(
   ylab="",
 )
 
+par(new=TRUE)
+plot(
+  xx, cos(3*xx),
+  xlim=c(-4,4),
+  ylim=c(-1.5,1.5),
+  type="l",
+  col="green",
+  xlab="",
+  ylab="",
+)
 
-legend(-4,1.5, legend=c("cos(x)", "cos(2x)"), col=c("blue","red"), lty=1)
+
+legend(-4,1.5, legend=c("cos(x)", "cos(2x)","cos(3x)"), col=c("blue","red","green"), lty=1)
 
 
 
