062df64b49aa67ea19a7b80d99908dcd1a977016
czylabsonasa <czylabson.asa@gmail.com>
2021-03-04 06:56:30 +0100

    progress

--- optAlg/marc3/fun5.m
+++ optAlg/marc3/fun5.m
@@ -3,19 +3,19 @@
 f = @(x,y) 100*(y-x.^2).^2 + (1-x).^2 ;
 fV = @(v) f(v(1),v(2)) ;
 
-dfx = @(x,y) -400*x.*(y-x.^2).^2 + 2*(x-1) ;
+dfx = @(x,y) -400*x.*(y-x.^2) + 2*(x-1) ; % hoppá: itt benne maradt egy négyzet!!!
 dfy = @(x,y) 200*(y-x.^2) ;
 
 dfV =@(v) [dfx(v(1),v(2)) ; dfy(v(1),v(2)) ] ;
 d2fV = @(v) [ 1200*v(1)^2-400*v(2)+2, -400*v(1) ; -400*v(1), 200] ;
 
 % a tartomany
-xmin = -1 ; xmax = 3 ;
+xmin = -1.5 ; xmax = 2 ;
 xb = linspace(xmin,xmax,50) ;
-ymin = -1 ; ymax = 3 ;
+ymin = -3.5 ; ymax = 4 ;
 yb = linspace(ymin,ymax,50) ;
 
 % név
 fname="Rosenbrock" ;
-% pontok = [ 0.5 0.5;   0.7 0.9;  -1.2 1; rand() rand() ]' ;
-pontok = [ -1.2  1 ]' ;
+pontok = [ 0.5 0.5;   0.7 0.9;  -1.2 1; rand() rand() ]' ;
+% pontok = [ -1.2  1 ]' ;
--- optAlg/marc3/newton0.m
+++ optAlg/marc3/newton0.m
@@ -18,64 +18,68 @@ fprintf("függvény:\n\t%s\n",fname);
 fig = 0 ; % figure
 for pt=pontok
   x0 = pt ;
-  hold off ;
+  
   fprintf('-----------------------------------------\n') ;
   fprintf("x0:\n\t(%.3f,%.3f)\n", x0(1), x0(2)) ;
 
-  [loc, val, flag, out] = fminsearch(fV, x0) ;
+  [loc, ~, flag, out] = fminsearch(fV, x0) ;
   fprintf("fminsearch:\n\t(%.3f,%.3f) flag=%d nstep=%d funcCount=%d\n",...
     loc(1), loc(2), flag, out.iterations, out.funcCount ) ;
 
   
-  [loc, val, flag, out] = fminunc(fV, x0, optimoptions('fminunc','Display','none') ) ;
+  [loc, ~, flag, out] = fminunc(fV, x0, optimoptions('fminunc','Display','none') ) ;
   fprintf("fminunc:\n\t(%.3f,%.3f) flag=%d nstep=%d funcCount=%d\n",...
     loc(1), loc(2), flag, out.iterations, out.funcCount ) ;
 
 
+  hold off ;
   fig = fig + 1 ;
   figure(fig) ;
   clf ;
+  
   % kontur kirajzolasa minden fig-re
   contour(X,Y,f(X,Y)) ;
+  tit = sprintf('(%.3f,%.3f)',x0(1),x0(2));
+  title(tit) ;
+
   hold on ;
-  axis equal ;
 
   f0 = fV(x0) ;
   df0 = dfV(x0) ;
 
 
-  plot(x0(1), x0(2), 'xk') ;
-  amax = 10 ; % ad-hoc
+  plot(x0(1), x0(2), 'ok') ; axis square ;
+  
   nit = 0 ;
-  fcount = 2 ;
+  fcount = 1 + 2 ;
   flag = "none" ;
 
   while true
     nit = nit + 1 ;
-    if nit>maxit flag = "maxit" ; break ; end
+    if nit>maxit, flag = "maxit" ; break ; end
 
-%    alfa = inv(d2fV(x0)) ; 
-
-    d2fV(x0)
     pause();
     p = d2fV(x0) \ (-df0) ;
     x1 = x0 + p ;
-    % x1 = x0 - alfa*df0 ;
     f1 = fV(x1) ;
     df1 = dfV(x1) ;
-    fcount = fcount + 4 ; % es az invertalast nem is szamoltuk
-    plot([x0(1),x1(1)], [x0(2),x1(2)], '-xk') ;
+    fcount = fcount + 1+2+4 ; % 
+    plot([x0(1),x1(1)], [x0(2),x1(2)], '-xk') ; axis square ;
 
 
-    if norm(df1)<dftol, flag = "dftol" ; end
-    if abs(f1-f0)<ftol, flag = "ftol" ; end
-    if norm(x1-x0)<xtol, flag = "xtol" ; end
+    if norm(df1)<dftol
+      flag = "dftol" ;
+    elseif abs(f1-f0)<ftol
+      flag = "ftol" ;
+    elseif norm(x1-x0)<xtol
+      flag = "xtol" ;
+    end
     x0 = x1 ;
     f0 = f1 ;
     df0 = df1 ;
     if not (flag == "none"), break; end
   end
-
+  
   fprintf("newton0:\n\t(%.3f,%.3f) flag=%s nstep=%d funcCount=%d\n",...
     x0(1), x0(2), flag, nit, fcount ) ;
 
