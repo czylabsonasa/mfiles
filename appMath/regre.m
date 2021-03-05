19102974a444c61824f27b258ca3db803ae68038
czylabsonasa <czylabson.asa@gmail.com>
2021-03-05 11:44:57 +0100

    progress

--- appMath/regre.m
+++ appMath/regre.m
@@ -1,14 +1,50 @@
-t = [1 5 6 7 8]' ;
-f = [411 5049 7137 9587 12413]' / 100 ;
+clear; 
+close ;
+clf ;
+
+t = [0 2 3 8 8]' ;
+f = [93 324 428 997 1000]' / 100 ;
 
 plot(t,f,'*') ;
 xlim([min(t)-1, max(t)+1]) ;
 ylim([min(f)-3, max(f)+3]) ;
 
 % polyfit
-p = polyfit(t, f, 2) ;
-polyval(p, 9)
+p = polyfit(t, f, 1) ;
+ertek= polyval(p, 10)
+
+v = polyval(p, t) - f ;
+delta = v'*v
 
 xx = linspace(min(t)-1, max(t)+1) ;
 hold on ;
 plot(xx, polyval(p, xx)) ;
+
+
+A = [ ones(size(t)), t ] ;
+alfa = A'*A \ A'*f ;
+
+[flip(alfa), p']
+
+% F = @(x) alfa(1) + alfa(2)*sin(pi*x) + alfa(3)*cos(pi*x);
+F = @(x) alfa(1) + alfa(2)*x ;
+
+F(10)
+
+% xx = linspace(min(t)-1, max(t)+1) ;
+% hold on ;
+% plot(xx, F(xx)) ;
+
+
+% M= A'A
+M = [length(t), sum(t); sum(t), sum(t.^2)] ;
+alfa = M \ [sum(f); sum(t.*f)]
+
+
+
+
+
+
+
+
+
