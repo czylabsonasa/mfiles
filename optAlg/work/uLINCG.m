bac2787fa55e0113da69999e0c7a08fc231b9328
czylabsonasa <czylabson.asa@gmail.com>
2021-04-14 13:56:37 +0200

    progress

---
+++ optAlg/work/uLINCG.m
@@ -0,0 +1,16 @@
+function [xx,flag]=uLINCG(A,b,c,x,maxit,epsi)
+  % a painless-ben leírtak szerint
+  % 1/2*x'Ax - x'b + c alakú fv-re!
+
+  fprintf("\nmódszer = konjugált gradiens (kvadratikus fv-re)" );
+
+  xx = [x];
+
+  flag = "none" ;
+  it = 0 ;
+
+  while true
+    ...
+  end
+  
+end
