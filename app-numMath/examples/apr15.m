feb85e181406796f262f0f0287a5b0e309a00d20
czylabsonasa <czylabson.asa@gmail.com>
2021-04-15 08:55:51 +0200

    progress

--- app-numMath/examples/apr15.m
+++ app-numMath/examples/apr15.m
@@ -3,15 +3,29 @@
 xx=linspace(-5,5);
 
 
-f=@(x) cos(x)-x;
-
+% f=@(x) cos(x)-x;
+f=@(x) exp(x) - 4*x.^2;
+df=@(x) exp(x) - 8*x;
 
 
 plot(xx,f(xx),[-5,5],[0,0]);
 
-bisect(f,0,2,20)
-fzero(f,0)
+%bs=bisect(f,0,1,20)
 
+n=newton(f,df,0.5,7)
+fz=fzero(f,0.5)
+abs(fz-n)
+
+
+
+
+
+
+function x=newton(f,df,x,step)
+  for i=1:step
+    x=x-f(x)/df(x);
+  end
+end
 
 
 function m=bisect(f,a,b,step)
