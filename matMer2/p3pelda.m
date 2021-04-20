98859ae9b9ff698ae11601a7bfa1b0de45a1ee76
czylabsonasa <czylabson.asa@gmail.com>
2021-04-20 18:25:45 +0200

    progress

--- matMer2/p3pelda.m
+++ matMer2/p3pelda.m
@@ -2,24 +2,24 @@ clf;
 clear;
 
 syms x;
-y=piecewise(-4<x<0,12,x==0,14,0<x<4,16);
+f=piecewise(-5<x<0,-3,x==0,3,0<x<5,9);
 
-xx=linspace(-4,4,300);
+xx=linspace(-10,10,400);
 
-lepesek=[5,10,15];
-nf=length(lepesek);
-for it=1:nf
-   subplot(nf,1,it);
-   fplot(y);
+tagok=[5,10,15];
+nt=length(tagok);
+for it=1:nt
+   subplot(nt,1,it);
+   fplot(f);
    hold on;
-   plot(xx,psum(xx,lepesek(it)))
-   title(sprintf('n=%d',lepesek(it)));
+   plot(xx,furi(xx,tagok(it)))
+   title(sprintf('n=%d',tagok(it)));
 end
 
-function ret=psum(t,n)
+function ret=furi(t,n)
    ret=zeros(size(t));
    for it=1:n
-      ret=ret+sin((2*it-1)*pi/4*t)/(2*it-1);
+      ret=ret+sin((2*it-1)*pi/5*t)/(2*it-1);
    end
-   ret=2*4/pi*ret+14;
+   ret=3+6*4/pi*ret;
 end
