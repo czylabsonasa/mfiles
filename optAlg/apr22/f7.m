86fb1e4d637ea58c4cd1e0396ecadf668db499b6
czylabsonasa <czylabson.asa@gmail.com>
2021-04-22 15:35:17 +0200

    progress

--- optAlg/apr22/f7.m
+++ optAlg/apr22/f7.m
@@ -2,24 +2,30 @@ clear;
 clf;
 close all;
 
-t=0:0.5:10;
-t=t';
+td=0:0.5:10;
+td=td';
 
-y=[-0.155 -1.079 0.086 0.887...
+yd=[-0.155 -1.079 0.086 0.887...
   -0.037 -0.728 0.003 0.596 0.020...
   -0.487 -0.035 0.397 0.043 -0.324 ...
   -0.048  0.264 0.049 -0.214 -0.048 0.173 0.046];
-y=y';
+yd=yd';
 
-length(t)
-length(y)
+length(td)
+length(yd)
 
-fun=@(x,T) x(1)*exp(x(2)*T).*cos(x(3)*T+x(4));
+fun=@(x,t) x(1)*exp(x(2)*t).*cos(x(3)*t+x(4));
 
-par=lsqcurvefit(fun,[1,1,1,1]',t,y);
+par=lsqcurvefit(fun,[1,1,1,1]',td,yd)
 
-sol=@(T) fun(par,T);
+fun2=@(x) fun(x,td)-yd
+par2=lsqnonlin(fun2,[1,1,1,1]')
+
+norm(par-par2)
+
+
+sol=@(t) fun(par,t);
 xx=linspace(-0.5,10.5);
-plot(t,y,'r*',xx,sol(xx),'b')
+plot(td,yd,'r*',xx,sol(xx),'b')
 
-norm(sol(t)-y)
+norm(sol(td)-yd)
