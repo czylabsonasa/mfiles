1df019f439ce2802b8b6aa99f0ae47f489c03642
czylabsonasa <czylabson.asa@gmail.com>
2021-04-22 14:46:55 +0200

    progress

---
+++ optAlg/apr22/f4.m
@@ -0,0 +1,55 @@
+% a 4. feladat generalt adattal
+
+clear; 
+clf;
+close all;
+
+n=200;
+
+t=linspace(0,4*pi,n)';
+%y=sin(t)+sin(2*t);
+y=sin(t)+t.*sin(2*t);y=sin(t)+t.*sin(2*t);
+
+
+
+megvan=70; % ez százalék
+
+van=rand(n,1)<megvan/100; 
+hiany=~van;y=sin(t)+t.*sin(2*t);
+
+
+tv=t(van);
+yv=y(van);
+th=t(hiany);
+yh=t(~hiany);
+
+subplot(3,1,1);
+plot(tv,yv,'k*',t,y,'b')
+xlim([0,4*pi]);
+
+S=eye(n);
+Sv=S(van,:); % S van
+Sn=S(hiany,:); % S hianyzik
+
+% elsorendu
+D=diag(ones(n,1))+diag(-1*ones(n-1,1),1);
+
+% masodrendu
+%D=diag(ones(n,1))+diag(-2*ones(n-1,1),1)+diag(ones(n-2,1),2);
+
+
+A=D*Sn';
+b=D*Sv'*yv;
+yh=(A'*A)\(-A'*b);
+
+subplot(3,1,2);
+plot(th,yh,'r*',t,y,'b')
+xlim([0,4*pi]);
+
+
+subplot(3,1,3);
+plot(tv,yv,'k*',th,yh,'r*',t,y,'b')
+xlim([0,4*pi]);
+
+
+
