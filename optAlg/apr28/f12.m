6a013bd8dda75905e5a58b5fc73aeeeffddcb471
czylabsonasa <czylabson.asa@gmail.com>
2021-04-28 15:30:00 +0200

    progress

---
+++ optAlg/apr28/f12.m
@@ -0,0 +1,30 @@
+% 12. feladat
+
+clear;
+clf;
+close all;
+
+
+n=200;
+t=linspace(0,10,n);
+t=t';
+y0=sin(2*t)+sin(3*t);
+yz=y0+0.1*randn(size(y0));
+
+plot(t,y0,'b', t, yz, 'r' )
+
+lam=10;
+D=diag(-ones(1,n-1),1)+eye(n);
+D=D(1:end-1,:);
+A=[eye(n); sqrt(lam)*D];
+b=[yz; zeros(n-1,1)];
+x=(A'*A)\(A'*b);
+
+hold on
+plot(t,x,'k')
+
+
+
+
+
+
